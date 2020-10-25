9c812e05ea25019d993213dae4f8b2f009fd8e14
nicholasctruong <cole.c1519@gmail.com>
2020-10-24 21:06:55 -0700

    first commit

---
+++ kyoko.py
@@ -0,0 +1,49 @@
+import os
+import random
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import statements
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='compliment', help='Compliments the user that you tag, add "aloud" to compliment in tts')
+async def compliment(ctx, target: discord.Member, tts=None):
+    if target == bot.user:
+        target = ctx.author
+        await ctx.send('awe thanks!')
+    compliments = statements.compliments
+    response = random.choice(compliments)
+    if tts == 'aloud':
+        await ctx.send(f'{target.nick}, {response}', tts=True)
+    else:
+        await ctx.send(f'{target.nick}, {response}')
+
+@bot.command(name='insult', help='Insults the user that you tag, add "aloud" to insult in tts')
+async def insult(ctx, target: discord.Member, tts=None):
+    if target == bot.user:
+        target = ctx.author
+        await ctx.send('damn you really tried that and failed.')
+    insults = statements.insults
+    response = random.choice(insults)
+    if tts == 'aloud':
+        await ctx.send(f'{target.nick}, {response}', tts=True)
+    else:
+        await ctx.send(f'{target.nick}, {response}')
+
+
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.MemberNotFound):
+        await ctx.send(f'Sorry! user not found, please try again')
+
+bot.run(TOKEN)
